280181998d73c16ecf6bdfa519e88fd6cfef875a
Josef Friedrich <josef@friedrich.rocks>
2022-12-31 11:18:59 +0100

    Add first text for the documentation

--- farbe.lua
+++ farbe.lua
@@ -1,6 +1,21 @@
-
+-- farbe.lua
+-- Copyright 2022 Josef Friedrich
+--
+-- This work may be distributed and/or modified under the
+-- conditions of the LaTeX Project Public License, either version 1.3c
+-- of this license or (at your option) any later version.
+-- The latest version of this license is in
+--   http://www.latex-project.org/lppl.txt
+-- and version 1.3c or later is part of all distributions of LaTeX
+-- version 2008/05/04 or later.
+--
+-- This work has the LPPL maintenance status `maintained'.
+--
+-- The Current Maintainer of this work is Josef Friedrich.
+--
+-- This work consists of the files farbe.lua, farbe.tex,
+-- and farbe.sty.
 ---@class Node
-
 -- https://github.com/latex3/xcolor/blob/main/xcolor.dtx
 local colors = {
   -- base colors
@@ -703,7 +718,7 @@ local function load_utils_functions()
 end
 
 --- https://github.com/Firanel/lua-color/blob/master/init.lua
-local function load_main_color_class()
+local Color = (function()
 
   --- Parse, convert and manipulate color values.
   --
@@ -1721,9 +1736,7 @@ local function load_main_color_class()
 
   return Color
 
-end
-
-local Color = load_main_color_class()
+end)()
 
 return {
   Color = Color,
