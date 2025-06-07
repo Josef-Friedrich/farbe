8db203e9a3c4515207da193f42e337a3cf12900d
Josef Friedrich <josef@friedrich.rocks>
2025-06-07 09:47:07 +0200

    test: Split test functions

--- tests/lua/test-class-color.lua
+++ tests/lua/test-class-color.lua
@@ -5,15 +5,7 @@ local farbe = require('farbe')
 local Color = farbe.Color
 
 describe('Class “Color”', function()
-  it('Constructor', function()
-    local color = Color('#ffffff')
-    assert.is.equal(color.r, 1.0)
-    assert.is.equal(color.g, 1.0)
-    assert.is.equal(color.b, 1.0)
-    assert.is.equal(color.a, 1.0)
-  end)
-
-  it('Constructor', function()
+  describe('Constructor', function()
     local black = Color('#000')
     local white = Color('#fff')
     local red = Color('#f00')
@@ -23,20 +15,53 @@ describe('Class “Color”', function()
     local cyan = Color('#0ff')
     local magenta = Color('#f0f')
 
-    assert.is.equal(Color('cmyk(100%, 100%, 100%, 100%)'), black)
-    assert.is.equal(Color('cmyk(1, 1, 1, 1)'), black)
-    assert.is.equal(Color('cmyk(1.0, 1.0, 1.0, 1.0)'), black)
-    assert.is.equal(Color('cmyk(0, 0, 0, 0)'), white)
-    assert.is.equal(Color('cmyk(0, 1, 1, 0)'), red)
-    assert.is.equal(Color('cmyk(0, 1, 0, 0)'), magenta)
+    it('hex', function()
+      local color = Color('#ffffff')
+      assert.is.equal(color.r, 1.0)
+      assert.is.equal(color.g, 1.0)
+      assert.is.equal(color.b, 1.0)
+      assert.is.equal(color.a, 1.0)
+    end)
 
-    assert.is.equal(Color('rgb(0,0,0)'), black)
-    assert.is.equal(Color('rgb(0.0,0.0,0.0)'), black)
-    assert.is.equal(Color('rgb(255, 255, 255)'), white)
-    assert.is.equal(Color('black'), black)
-    assert.is.equal(Color('white'), white)
 
+    describe('css style functions as string', function()
+      it('rgb(r, g, b)', function()
+        assert.is.equal(Color('rgb(0,0,0)'), black)
+        assert.is.equal(Color('rgb(0.0,0.0,0.0)'), black)
+        assert.is.equal(Color('rgb(255, 255, 255)'), white)
+      end)
+
+      it('rgba(r, g, b, a)', function() end)
+
+      it('hsl(h, s, l)', function() end)
+
+      it('hsla(h, s, l, a)', function() end)
+
+      it('hsv(h, s, v)', function() end)
+
+      it('hsva(h, s, v, a)', function() end)
+
+      it('hwb(h, w, b)', function() end)
+
+      it('hwba(h, w, b, a)', function() end)
+
+      it('cmyk(c, m, y, k)', function()
+        assert.is.equal(Color('cmyk(100%, 100%, 100%, 100%)'), black)
+        assert.is.equal(Color('cmyk(1, 1, 1, 1)'), black)
+        assert.is.equal(Color('cmyk(1.0, 1.0, 1.0, 1.0)'), black)
+        assert.is.equal(Color('cmyk(0, 0, 0, 0)'), white)
+        assert.is.equal(Color('cmyk(0, 1, 1, 0)'), red)
+        assert.is.equal(Color('cmyk(0, 1, 0, 0)'), magenta)
+      end)
+    end)
+
+    it('Color names', function()
+      assert.is.equal(Color('black'), black)
+      assert.is.equal(Color('white'), white)
+    end)
   end)
+
+
 
   it('Method cmyk()', function()
     local color = Color('#000000')
@@ -46,5 +71,4 @@ describe('Class “Color”', function()
     assert.is.equal(y, 0.0)
     assert.is.equal(k, 1.0)
   end)
-
 end)
